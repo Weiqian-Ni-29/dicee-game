e73814bd3ab36022589416031bd921e5d9acd522
Weiqian-Ni-29 <101934090+Weiqian-Ni-29@users.noreply.github.com>
2022-08-02 19:20:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelectorAll("img")[0].setAttribute("src",'images/dice'+ randomNumber1 + '.png');
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;;
+document.querySelectorAll("img")[1].setAttribute("src",'images/dice'+ randomNumber2 + '.png');
